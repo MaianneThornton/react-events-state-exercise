01f020d2c5d4a1548155bde8eb75e3b14cf5ce0e
MaianneThornton <maianne.thornton@gmail.com>
2021-11-16 13:13:53 -0500

    Completed exercise. PRE BONUS

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+// 1d. Open the App.js file and add the necessary import line at the top of the file that allows the Exercise component to be used in the App.js file
+import Exercise from "./exercise/Exercise"
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Exercise />
   );
 }
 
