db6ff31bd6bb94f39d8fecb6ce917c52533e91d8
MaianneThornton <maianne.thornton@gmail.com>
2021-11-17 11:10:33 -0500

    Finished Bonus in class. Exercise is complete

--- src/exercise/Exercise.js
+++ src/exercise/Exercise.js
@@ -4,8 +4,6 @@ import { useState } from 'react';
 
 // 1a. Create a function component called Exercise
 function Exercise (){
-  // 1c. Inside the Exercise function component, use the RETURN keyword with parentheses to have the Exercise function component return a button element and set the text inside of it to "CLICK HERE"
-
   //2c. Above the return keyword in the Exercise function component, use the "useState" function with the number 1 passed in as an argument. Also, use destructuring to set the values of the array that are returned from the useState function to the following const variable names…
   // First name: "num"
   // Second name: "setNum"
@@ -13,16 +11,14 @@ function Exercise (){
   const [num, setNum] = useState(1);
 
   // 2d. Below useState function, create a function called addNum that adds one to the value of the num variable and sets that as the new state value for the num variable by using the setNum function inside of the addNum function
-  const addNum = () => {
-    setNum(num + 1);
-  };
-
+  const addNum = () => setNum(num + 1);
+  
+  // 1c. Inside the Exercise function component, use the RETURN keyword with parentheses to have the Exercise function component return a button element and set the text inside of it to "CLICK HERE"
   return (
     <div>
       {/* 1e. Use the "onClick" React event listener on the button and set the value for the "onClick" React event listener to a function that alerts the user with the following message when the button is clicked…
       "Congrats! You have clicked the button."
       (HINT: Remember to use brackets {}) */}
-
     <button onClick = {() => alert ("Congrats! You have clicked the button.")}>CLICK HERE </ button>
     {/* 2b. Create another button element below the "CLICK HERE" button in the Exercise function component set the text inside of it to "STATE" */}
     {/* 2e. Use the "onClick" React event listener on the "STATE" button and set the value for the "onClick" React event listener to the addNum function (HINT: Remember to use brackets {}) */}
